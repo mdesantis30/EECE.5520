970e1c073d9d4aef375c983022b44ce247f93e76
Matthew DeSantis <mdesantis0730@gmail.com>
2024-02-08 12:01:11 -0500

    Program Setup
    Adding the title, setup and loop functions.

---
+++ Lab1EECE5520/Lab1EECE5520.ino
@@ -0,0 +1,15 @@
+// EECE.5520 Microprocessors II
+// Matthew DeSantis and Ryan Mouhib
+// Lab 1 - Traffic Lights
+// This program is designed to simulate the timing and execution of a traffic light system.
+// For the graduate section, this program will execute faster light blinking, intersection traffic lights, and a 7 segment display for the timer.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
