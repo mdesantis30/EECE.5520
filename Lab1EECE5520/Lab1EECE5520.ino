c256c2a42f6a7098b5c963c52fc36664527f8214
Matthew DeSantis <mdesantis0730@gmail.com>
2024-02-16 19:28:59 -0500

    Timer Interrupt Test
    testing the timer code

--- Lab1EECE5520/Lab1EECE5520.ino
+++ Lab1EECE5520/Lab1EECE5520.ino
@@ -1,15 +1,77 @@
-// EECE.5520 Microprocessors II
-// Matthew DeSantis and Ryan Mouhib
-// Lab 1 - Traffic Lights
-// This program is designed to simulate the timing and execution of a traffic light system.
-// For the graduate section, this program will execute faster light blinking, intersection traffic lights, and a 7 segment display for the timer.
+// timer interrupt sample
+// yluo modified for lab1 of EECE.4520/5520
+// 9/12/2021
+//
+// original source:-
+//timer interrupts
+//by Amanda Ghassaei
+//June 2012
+//https://www.instructables.com/id/Arduino-Timer-Interrupts/
 
-void setup() {
-  // put your setup code here, to run once:
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+*/
 
+//timer setup for timer0, timer1, and timer2.
+//For arduino uno or any board with ATMEL 328/168.. diecimila, duemilanove, lilypad, nano, mini...
+
+//this code will enable one arduino timer interrupt
+//timer1 will interrupt at 1Hz
+
+//storage variables
+boolean toggle1 = 0;
+
+void setup(){
+  
+  //set pins as outputs
+  pinMode(13, OUTPUT);
+
+  cli();//stop interrupts
+
+  //set timer1 interrupt at 1Hz
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS12 and CS10 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+
+  sei();//allow interrupts
+
+}//end setup
+
+// Timer1's interrupt service routing (ISR)
+// The code in ISR will be executed every time timer1 interrupt occurs
+// That is, the code will be called once every second
+// TODO
+//   you need to set a flag to trigger an evaluation of the conditions
+//   in your state machine, then potentially transit to next state
+//
+ISR(TIMER1_COMPA_vect){//timer1 interrupt 1Hz toggles pin 13 (LED)
+//generates pulse wave of frequency 1Hz/2 = 0.5kHz (takes two cycles for full wave- toggle high then toggle low)
+  if (toggle1){
+    digitalWrite(13,HIGH);
+    toggle1 = 0;
+  }
+  else{
+    digitalWrite(13,LOW);
+    toggle1 = 1;
+  }
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+  
+void loop(){
+  //do other things here
+  if (toggle1) {
+  }
+  else {
+  }
 }
