bfaf1bb453ffcdb49497eb4dad3f2aec4d4ee727
ryanmouhib <114774177+ryanmouhib@users.noreply.github.com>
2024-02-19 10:03:49 -0500

    Working_synchronized_traffic lights.ino
    - Synchronized traffic lights
    - Buzzer beep working

--- Lab1EECE5520/Working_synchronized_traffic lights.ino
+++ Lab1EECE5520/Working_synchronized_traffic lights.ino
@@ -28,14 +28,14 @@ void blinkLED(int pin) // the pin parameter determines which LEDs will blink
   endTime = startTime;
   digitalWrite(buzzer, HIGH);
   while ((endTime - startTime) <= 3000) { // loops for 3 seconds
-    digitalWrite(latchPin, LOW);  // flashing specified LED repeatedly by first turning it on for 0.5 seconds then off for 0.5 seconds
-    shiftOut(dataPin, clockPin, MSBFIRST, pin);
+    digitalWrite(latchPin, LOW);  // flashing specified LEDs repeatedly by first turning it on for 0.5 seconds then off for 0.5 seconds
+    shiftOut(dataPin, clockPin, MSBFIRST, pin); // pins are specified when calling blinkLED function in traffic lights loop
     digitalWrite(latchPin, HIGH); // LEDs on
-    delay(500); // 0.5 second delay while LED is on
+    delay(500); // 0.5 second delay while LEDs are on
     digitalWrite(latchPin, LOW);
     shiftOut(dataPin, clockPin, MSBFIRST, 0b00000000);
     digitalWrite(latchPin, HIGH); // LEDs off
-    delay(500); // 0.5 second delay while LED is off
+    delay(500); // 0.5 second delay while LEDs are off
     loopcount = loopcount+1;
     endTime = millis();
   }
@@ -50,14 +50,14 @@ void loop()
   shiftOut(dataPin, clockPin, MSBFIRST, 0b00011000); // the GLA LEDs are synchronized for both traffic lights
   digitalWrite(latchPin, HIGH); // turning GLA LEDs on
   delay(2000);  // GLA stays on full for 2 seconds before flashing for 3 seconds 
-  blinkLED(0b00011000); // calling function to blink GLA LED and buzzer beep for 3 seconds
+  blinkLED(0b00011000); // calling function to blink GLA LEDs and buzzer beep for 3 seconds
   
   // Green stays on for 12 seconds
   digitalWrite(latchPin, LOW);
   shiftOut(dataPin, clockPin, MSBFIRST, 0b00100100); // the green LEDs are synchronized for both traffic lights
   digitalWrite(latchPin, HIGH); // turning green LEDs on
   delay(9000); // green stays on full for 9 seconds before flashing for 3 seconds 
-  blinkLED(0b00100100); // calling function to blink green LED and buzzer beep for 3 seconds
+  blinkLED(0b00100100); // calling function to blink green LEDs and buzzer beep for 3 seconds
 
   // Yellow stays on for 3 seconds
   digitalWrite(buzzer, HIGH);
@@ -72,7 +72,7 @@ void loop()
   shiftOut(dataPin, clockPin, MSBFIRST, 0b10000001); // the red LEDs are synchronized for both traffic lights
   digitalWrite(latchPin, HIGH); // turning red LEDs on
   delay(17000); // green stays on full for 17 seconds before flashing for 3 seconds 
-  blinkLED(0b10000001); // calling function to blink red LED and buzzer beep for 3 seconds
+  blinkLED(0b10000001); // calling function to blink red LEDs and buzzer beep for 3 seconds
 }
 
 // Sources:
