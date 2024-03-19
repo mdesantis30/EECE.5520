a8b4dd23d3efbe053087d09e0f15f2e82bedca83
ryanmouhib <114774177+ryanmouhib@users.noreply.github.com>
2024-03-19 19:36:31 -0400

    Working_accelerometer.ino

---
+++ Lab2EECE5520/Working_accelerometer.ino
@@ -0,0 +1,68 @@
+#include<Wire.h>
+
+const int MPU_addr=0x68;  // I2C address of the MPU-9250
+int16_t AcZ,GyX,GyY;
+
+const int buzzer = 13; // pin of the active buzzer
+
+void setup(){
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-9250)
+  Wire.endTransmission(true);
+  Serial.begin(9600);
+  pinMode(buzzer, OUTPUT); //Initialize the buzzer pin as an output
+}
+
+void loop(){
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 4 registers
+
+  // variables accessing readings for gyroscope function of the MPU-9250
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+
+  // variables accessing readings for accelerometer function of the MPU-9250
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  
+  // Accelerometer
+  Serial.print("AcZ = "); Serial.print(AcZ); Serial.print("\n");
+
+  // When shaking is detected, values returned from AcZ for the z-axis go below 11000
+  if (AcZ <= 11000)
+  {
+    Serial.println("Shake detected"); // Send a message over serial port to notify code in python that sensor has been shaken
+  }
+  
+  /*
+  if (GyY >= 5000)
+  {
+    Serial.print("Moves down\n");
+    Serial.print(GyY);
+    Serial.print("\n\n");
+  }
+  else if (GyY <= -5000)
+  {
+    Serial.print("Moves up\n");
+    // Serial.print(GyY);
+    // Serial.print("\n\n");
+  }
+  else if (GyX <= -5000)
+  {
+    Serial.print("Moves right\n");
+    // Serial.print(GyX);
+    // Serial.print("\n\n");
+  }
+  else if (GyX >= 5000)
+  {
+    Serial.print("Moves left\n");
+    // Serial.print(GyX);
+    // Serial.print("\n\n");
+  }*/
+
+  delay(100); // update every 0.1 seconds
+
+}
