3f13632512dd972bdb2312c78740032d9d05bbf1
ryanmouhib <114774177+ryanmouhib@users.noreply.github.com>
2024-03-18 23:58:16 -0400

    Added calibration

--- Lab2EECE5520/Working_joystick.ino
+++ Lab2EECE5520/Working_joystick.ino
@@ -8,6 +8,7 @@
 const int SW_pin = 2; // digital pin connected to switch output
 const int X_pin = A0; // analog pin connected to X output, returns an integer from 0-1023
 const int Y_pin = A1; // analog pin connected to Y output, returns an integer from 0-1023
+const int buzzer = 13; // pin of the active buzzer
 
 // variables for analog reading
 int X_read;
@@ -20,9 +21,10 @@ int UD_high;
 int UD_low;
 
 void setup() {
-  pinMode(SW_pin, INPUT);
-  digitalWrite(SW_pin, HIGH);
   Serial.begin(9600);
+
+  pinMode(buzzer, OUTPUT); //Initialize the buzzer pin as an output
+
   // leaving a +/- 5 degree angle dead zone to determine the joystick center and take into account its lack of precision
   // Important: assumes joystick is left untouched when code is initializing
   LR_high = analogRead(X_pin) + 5;
@@ -37,52 +39,32 @@ void loop() {
   
   if (Y_read >= UD_high)
   {
-    Serial.print("Moves down\n"); // Translate: move snake down
-    Serial.print("X-axis: ");
-    Serial.print(analogRead(X_pin));
-    Serial.print("\nY-axis: ");
-    Serial.println(analogRead(Y_pin));
-    Serial.print("\n");
+    Serial.print("Moves down\n");
   }
   else if (Y_read <= UD_low)
   {
-    Serial.print("Moves up\n"); // Translate: move snake up
-    Serial.print("X-axis: ");
-    Serial.print(analogRead(X_pin));
-    Serial.print("\nY-axis: ");
-    Serial.println(analogRead(Y_pin));
-    Serial.print("\n");
+    Serial.print("Moves up\n");
   }
   else if (X_read >= LR_high)
   {
-    Serial.print("Moves right\n"); // Translate: move snake right
-    Serial.print("X-axis: ");
-    Serial.print(analogRead(X_pin));
-    Serial.print("\nY-axis: ");
-    Serial.println(analogRead(Y_pin));
-    Serial.print("\n");
+    Serial.print("Moves right\n");
   }
   else if (X_read <= LR_low)
   {
-    Serial.print("Moves left\n"); // Translate: move snake left
-    Serial.print("X-axis: ");
-    Serial.print(analogRead(X_pin));
-    Serial.print("\nY-axis: ");
-    Serial.println(analogRead(Y_pin));
-    Serial.print("\n");
+    Serial.print("Moves left\n");
   }
-  else
-  {
-    Serial.print("Joystick is centered\n"); // Translate: stop moving snake
-    Serial.print("X-axis: ");
-    Serial.print(analogRead(X_pin));
-    Serial.print("\nY-axis: ");
-    Serial.println(analogRead(Y_pin));
-    Serial.print("\n");
-  }
-  delay(500);
-}
+  delay(100);
 
+  // Check if Python sent a signal indicating an apple is eaten
+  if (Serial.available() > 0) {
+    char signal = Serial.read();
+    if (signal == 'e') {
+      digitalWrite(buzzer, HIGH);// Turn on buzzer
+      delay(200);
+      digitalWrite(buzzer, LOW);// Turn off buzzer
+    }
+  }
+}
 
 // Sources:
 // Source files for Lab 2, Professor Yan Luo, University of Massachusetts Lowell
