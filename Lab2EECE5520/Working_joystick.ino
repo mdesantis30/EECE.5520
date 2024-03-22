8558376088cf208ccccb5b968c0773b20bc9b32d
ryanmouhib <114774177+ryanmouhib@users.noreply.github.com>
2024-03-22 07:27:09 -0400

    Working joystick with accelerometer

--- Lab2EECE5520/Working_joystick.ino
+++ Lab2EECE5520/Working_joystick.ino
@@ -4,15 +4,19 @@
 // and a buzzer which beeps when an apple is eaten, when the accelerometer (MPU-6050) is "shaked" the points double for the next apple. 
 // The game ends when the snake hits onto the boundary.
 
+#include<Wire.h>
+
+const int MPU_addr=0x68;  // I2C address of the MPU-9250
+int16_t AcX,AcY,AcZ;
+
 // Arduino pin numbers
-const int SW_pin = 2; // digital pin connected to switch output
 const int X_pin = A0; // analog pin connected to X output, returns an integer from 0-1023
 const int Y_pin = A1; // analog pin connected to Y output, returns an integer from 0-1023
 const int buzzer = 13; // pin of the active buzzer
 
 // variables for analog reading
-int X_read;
-int Y_read;
+int X_read; // x-axis
+int Y_read; // y-axis
 
 // Neutral readings to determine joystick calibration
 int LR_high;
@@ -25,6 +29,12 @@ void setup() {
 
   pinMode(buzzer, OUTPUT); //Initialize the buzzer pin as an output
 
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-9250)
+  Wire.endTransmission(true);
+
   // leaving a +/- 5 degree angle dead zone to determine the joystick center and take into account its lack of precision
   // Important: assumes joystick is left untouched when code is initializing
   LR_high = analogRead(X_pin) + 5;
@@ -34,25 +44,39 @@ void setup() {
 }
 
 void loop() {
-  // reading data from analog pins
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 4 registers
+
+  //variable accessing readings for accelerometer function of the MPU-9250
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+
   X_read = analogRead(X_pin);
   Y_read = analogRead(Y_pin);
   
+  if (AcZ <= 11000)
+  {
+    Serial.println("Shake detected"); // Send a message over serial port to notify code in python that sensor has been shaken
+  }
+
   if (Y_read >= UD_high)
   {
-    Serial.print("Moves down\n");
+    Serial.print("Moves down\n"); // Send a message over serial port to notify code in python that joystick moves down
   }
   else if (Y_read <= UD_low)
   {
-    Serial.print("Moves up\n");
+    Serial.print("Moves up\n"); // Send a message over serial port to notify code in python that joystick moves up
   }
   else if (X_read >= LR_high)
   {
-    Serial.print("Moves right\n");
+    Serial.print("Moves right\n"); // Send a message over serial port to notify code in python that joystick moves rifht
   }
   else if (X_read <= LR_low)
   {
-    Serial.print("Moves left\n");
+    Serial.print("Moves left\n"); // Send a message over serial port to notify code in python that joystick moves left
   }
   delay(100);
 
