105162a1b8ecfb18892dce94fcfce4ca85293823
ryanmouhib <114774177+ryanmouhib@users.noreply.github.com>
2024-04-09 13:06:32 -0400

    Update Lab3.ino

--- Lab3EECE5520/Lab3.ino
+++ Lab3EECE5520/Lab3.ino
@@ -6,16 +6,16 @@
 #include <arduinoFFT.h>
 
 // Ultrasonic sensor pins
-const int trigPin = 13; // 10 microsecond high pulse causes chirp , wait 50 us
+const int trigPin = 13;
 const int echoPin = 12; // Width of high pulse indicates distance
 
 // Motor control pins: L298N bridge
-const int enAPin = 6; // Left motor PWM speed control
-const int in1Pin = 7; // Left motor Direction 1
+const int enAPin = 2; // Left motor PWM speed control
+const int in1Pin = 3; // Left motor Direction 1
 const int in2Pin = 5; // Left motor Direction 2
-const int in3Pin = 4; // Right motor Direction 1
-const int in4Pin = 2; // Right motor Direction 2
-const int enBPin = 3; // Right motor PWM speed control
+const int in3Pin = 6; // Right motor Direction 1
+const int in4Pin = 7; // Right motor Direction 2
+const int enBPin = 8; // Right motor PWM speed control
 
 void setup()
 {
@@ -39,13 +39,13 @@ void setup()
 // Motor A (speedA and dirA) corresponds to the right motor
 // Motor B (speedB and dirB) correpsonds to the left motor
 // "true" means forward and "false" means backwards
-void setMotorSpeed(int speedA, int speedB, bool dirA, bool dirB) {
+void setMotorControl(int speedA, int speedB, bool dirA, bool dirB) {
   // Set motor A direction
   digitalWrite(in1Pin, dirA ? HIGH : LOW);
   digitalWrite(in2Pin, !dirA ? HIGH : LOW);
   // Set motor B direction
-  digitalWrite(in3Pin, dirB ? HIGH : LOW);
-  digitalWrite(in4Pin, !dirB ? HIGH : LOW);
+  digitalWrite(in3Pin, dirB ?  LOW : HIGH);
+  digitalWrite(in4Pin, !dirB ? LOW : HIGH);
   // Set motor speed
   analogWrite(enAPin, speedA);
   analogWrite(enBPin, speedB);
@@ -66,38 +66,40 @@ void loop()
 {
   unsigned int distance = readDistance(); // read distance from the ultrasonic sensor
   unsigned int desiredDistance = 100; // define desired distance from the object (trash can) to 200 mm
-  
-  setMotorSpeed(100, 100, true, true);
-  delay(10);
+  int motorSpeedA = 150;
+  int motorSpeedB = 150;
 
-  if (distance < 100) // If the measured distance is less than the desired distance turn slightly left
+  if (distance < 100) // If the measured distance is less than the desired distance minus the tolerance, turn slightly left
   {
     // Turn slightly left to check distance and shape of object being circled
-    setMotorSpeed(200, 200, true, false);
-    delay(100);
+    setMotorControl(motorSpeedA, motorSpeedB, false, true);
+    /*
     Serial.print("Car turning left.\n");
     Serial.print("Distance: ");
     Serial.print(distance);
     Serial.print("\n");
+    */
   }
-  else if (distance > 150) // If the measured distance is greater than the desired distance turn slightly right
+  else if (distance > 150) // If the measured distance is greater than the desired distance plus the tolerance, turn slightly right
   {
     // Turn slightly right
-    setMotorSpeed(200, 200, false, true);
-    delay(100);
+    setMotorControl(motorSpeedA, motorSpeedB, true, false);
+    /*
     Serial.print("Car turning right.\n");
     Serial.print("Distance: ");
     Serial.print(distance);
     Serial.print("\n");
+    */
   }
   else // If the measured distance is within the acceptable range, move forward
   {
     // Move forward
-    setMotorSpeed(200, 200, true, true);
-    delay(100);
+    setMotorControl(motorSpeedA, motorSpeedB, true, true);
+    /*
     Serial.print("Car moving forward.\n");
     Serial.print("Distance: ");
     Serial.print(distance);
     Serial.print("\n");
+    */
   }
 }
